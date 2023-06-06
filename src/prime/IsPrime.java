2aad90dbcfdb44aa5e4f2f7f05128989c2312600
Richard Agarici <laurentiu.agarici@gmail.com>
2023-06-06 17:41:44 +0200

    adding the function

--- src/prime/IsPrime.java
+++ src/prime/IsPrime.java
@@ -1,2 +1,20 @@
-package prime;public class IsPrime {
+package prime;
+public class IsPrime {
+    public static boolean isPrime(int number){
+        for(int i=2;i<number;i++){
+            if (number % i == 0) {
+                return false;
+                //means it is not prime
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        for (int i=2; i<200;i++){
+            if(isPrime(i)){
+        System.out.println(i);
+            }
+        }
+    }
 }
+
